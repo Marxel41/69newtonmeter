2e1297701543e3f9ad00f81b55578a274dce6d36
Marxel41 <76850771+Marxel41@users.noreply.github.com>
2026-01-07 22:35:57 +0100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // HIER DEINE URL EINFÜGEN!
-const API_URL = "https://script.google.com/macros/library/d/1OCuFCj1rU6lNomnFryDVttVzlOBToNBgBTinfKnK3BVkbN6imrP28qde/4";
+const API_URL = "https://script.google.com/macros/s/AKfycbzKCx9ZmwhkMPfROYPlkRUc8UKjdSS-QBtDv-MvOiqG1uFnwKPZPSTh4dIGS-MQa4wkow/exec";
 
 const API = {
     async post(action, data = {}) {
